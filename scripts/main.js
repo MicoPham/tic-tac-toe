1d0bdee1c64833cd7cd9977c6c49f91a1e84e628
MicoPham <michaelxpham@gmail.com>
2022-02-27 15:11:43 -0800

    Let players click on the gameboard to place their marker and tie it to DOM with logic that prevent players from playing in spots that are already taken

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,36 @@
-
+let playerFactory = (name, token) => {
+    return {name, token};
+}
 
 let gameBoard = (function() {
-    let boardArr = ['x','x','x','x','x','x','x','x','x'];
+    let boardArr = ['','','','','','','','',''];
+    let playerOne = playerFactory('Player 1', "x");
+    let playerTwo = playerFactory('Player 2', 'o');
+    let currentPlayer = playerOne;
     let render = () => {
         let board = document.querySelector('#board');
         for (let i = 0; i < boardArr.length; i++) {
             let tile = document.createElement('div');
             tile.setAttribute('class', 'tile');
             tile.textContent = boardArr[i];
+            if(tile.textContent === '') {
+                tile.addEventListener('click', currentPlayerClick);
+            }
             board.appendChild(tile);
         }
-    return board;    
+        return board;    
+    };
+    let currentPlayerClick = (e) => {
+        if(currentPlayer === playerOne){
+            e.target.textContent = playerOne.token;
+            boardArr[`e.target.id`] = playerOne.token;
+            currentPlayer = playerTwo;
+        } else if (currentPlayer === playerTwo){
+            e.target.textContent = playerTwo.token;
+            boardArr['e.target.id'] = playerTwo.token;
+            currentPlayer = playerOne;
+        }
+        e.target.removeEventListener('click', currentPlayerClick);
     }
     return {render};
 })();
