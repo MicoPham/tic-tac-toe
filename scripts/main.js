5d578c458da50749142f172613cd821b413fbac0
MicoPham <michaelxpham@gmail.com>
2022-02-28 17:10:53 -0800

    Build the logic that checks for when game is over. (Check for 3-in-a-row and a tie)

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ let playerFactory = (name, token) => {
     return {name, token};
 }
 
+
 let gameBoard = (function() {
     let boardArr = ['','','','','','','','',''];
     let playerOne = playerFactory('Player 1', "x");
@@ -12,6 +13,7 @@ let gameBoard = (function() {
         for (let i = 0; i < boardArr.length; i++) {
             let tile = document.createElement('div');
             tile.setAttribute('class', 'tile');
+            tile.setAttribute('id', i)
             tile.textContent = boardArr[i];
             if(tile.textContent === '') {
                 tile.addEventListener('click', currentPlayerClick);
@@ -19,20 +21,42 @@ let gameBoard = (function() {
             board.appendChild(tile);
         }
         return board;    
-    };
+    }
     let currentPlayerClick = (e) => {
         if(currentPlayer === playerOne){
             e.target.textContent = playerOne.token;
-            boardArr[`e.target.id`] = playerOne.token;
+            boardArr[e.target.id] = playerOne.token;
             currentPlayer = playerTwo;
         } else if (currentPlayer === playerTwo){
             e.target.textContent = playerTwo.token;
-            boardArr['e.target.id'] = playerTwo.token;
+            boardArr[e.target.id] = playerTwo.token;
             currentPlayer = playerOne;
         }
         e.target.removeEventListener('click', currentPlayerClick);
+        gameOver();
     }
+    function gameOver() {
+        return winCondition(0,1,2) || winCondition(3,4,5) || winCondition(6,7,8) || winCondition(0,3,6) || winCondition(1,4,7) || winCondition(2,5,8) || winCondition(0,4,8) || winCondition(2,4,6) || tieGame();
+    }
+    function winCondition(tile1, tile2, tile3){
+        let tokenAtOne = boardArr[tile1];
+        if (tokenAtOne === '') return false;
+        let tokenAtTwo = boardArr[tile2];
+        if (tokenAtOne !== tokenAtTwo) return false;
+        let tokenAtThree = boardArr[tile3];
+        if (tokenAtOne !== tokenAtThree) return false;
+        console.log(`${tokenAtOne} won`);
+    }
+    function tieGame(){
+        for (let i = 0; i < boardArr.length; i++) {
+            if (boardArr[i] === '') return false;
+        }  
+        console.log(`game over`);
+        return;
+    }
+
     return {render};
 })();
 
 gameBoard.render();
+
