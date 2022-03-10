8a479832890bb06444c096330391eb5bbda0e77d
MicoPham <michaelxpham@gmail.com>
2022-03-09 17:09:36 -0800

    Add user input, add start/restart btn, add display element

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,19 @@
-let playerFactory = (name, token) => {
-    return {name, token};
-}
-
-
 let gameBoard = (function() {
+    let playerOne = '', playerTwo = '', currentPlayer;
     let boardArr = ['','','','','','','','',''];
-    let playerOne = playerFactory('Player 1', "x");
-    let playerTwo = playerFactory('Player 2', 'o');
-    let currentPlayer = playerOne;
+    const display = document.querySelector('#display');
+    const board = document.querySelector('#board');
+    let playerFactory = (name, token) => {
+        return {name, token};
+    }
     let render = () => {
-        let board = document.querySelector('#board');
+        board.innerHTML = '';
         for (let i = 0; i < boardArr.length; i++) {
             let tile = document.createElement('div');
             tile.setAttribute('class', 'tile');
             tile.setAttribute('id', i)
             tile.textContent = boardArr[i];
-            if(tile.textContent === '') {
-                tile.addEventListener('click', currentPlayerClick);
-            }
+            tile.addEventListener('click', currentPlayerClick);
             board.appendChild(tile);
         }
         return board;    
@@ -39,24 +35,34 @@ let gameBoard = (function() {
         return winCondition(0,1,2) || winCondition(3,4,5) || winCondition(6,7,8) || winCondition(0,3,6) || winCondition(1,4,7) || winCondition(2,5,8) || winCondition(0,4,8) || winCondition(2,4,6) || tieGame();
     }
     function winCondition(tile1, tile2, tile3){
-        let tokenAtOne = boardArr[tile1];
-        if (tokenAtOne === '') return false;
-        let tokenAtTwo = boardArr[tile2];
-        if (tokenAtOne !== tokenAtTwo) return false;
-        let tokenAtThree = boardArr[tile3];
-        if (tokenAtOne !== tokenAtThree) return false;
-        console.log(`${tokenAtOne} won`);
+        if (boardArr[tile1] === '') return;
+        if (boardArr[tile1] !== boardArr[tile2]) return;
+        if (boardArr[tile1] !== boardArr[tile3]) return;
+        document.querySelectorAll('.tile').forEach((tile) => tile.removeEventListener('click', currentPlayerClick));
+        if (boardArr[tile1] === 'x') {
+            return display.innerText = `${playerOne.name} is the winner!`;
+        } else {
+            display.innerText = `${playerTwo.name} is the winner!`;
+        }
     }
     function tieGame(){
         for (let i = 0; i < boardArr.length; i++) {
-            if (boardArr[i] === '') return false;
-        }  
-        console.log(`game over`);
-        return;
+            if (boardArr[i] === '') return;
+        }
+        document.querySelectorAll('.tile').forEach((tile) => tile.removeEventListener('click', currentPlayerClick)); 
+        display.innerText = `It's a tie!`;
     }
-
+    startBtn.addEventListener('click', (e) => {
+        playerOne = playerFactory(document.getElementById('player1').value, "x");
+        playerTwo = playerFactory(document.getElementById('player2').value, 'o');
+        currentPlayer = playerOne;
+        document.querySelector('form').remove();
+        document.querySelector('#startBtn').remove();
+        display.innerText = `${playerOne.name} is ${playerOne.token} and ${playerTwo.name} is ${playerTwo.token}.`;
+        gameBoard.render();
+    })
+    restartBtn.addEventListener('click', (e) => {
+        location.reload();
+    })
     return {render};
 })();
-
-gameBoard.render();
-
